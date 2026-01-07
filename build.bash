2e666f397180c671b5ec4735da171290dc45ea7a
Gorag <greg6821@gmail.com>
2026-01-08 02:19:30 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,57 +1,44 @@
 set -e
 
 export DEVKITPRO=/opt/devkitpro
-export DEVKITA64=/opt/devkitpro/devkitA64
 
-# Явно указываем компилятор devkitPro
-export CC="$DEVKITA64/bin/aarch64-none-elf-gcc"
-export AR="$DEVKITA64/bin/aarch64-none-elf-ar"
-export RANLIB="$DEVKITA64/bin/aarch64-none-elf-ranlib"
-
-# Только флаги компиляции, без линковки
-export CFLAGS="-O2 -ffunction-sections -fdata-sections -D__SWITCH__ -I$DEVKITPRO/libnx/include -I/opt/devkitpro/portlibs/switch/include"
-export LDFLAGS=""
-
-cd cpython
-mkdir -p build-switch
-cd build-switch
-
-# Минимальная конфигурация
+source $DEVKITPRO/switchvars.sh
+pushd cpython
+mkdir build-switch
+cp ../cpython_config_files/config.site build-switch
+pushd build-switch
+mkdir local_prefix
+export LOCAL_PREFIX=$(realpath local_prefix)
 ../configure \
   --host=aarch64-none-elf \
+  --build=$(../config.guess) \
+  --prefix="$LOCAL_PREFIX" \
+  --disable-ipv6 \
   --disable-shared \
   --without-pymalloc \
-  --disable-ipv6 \
-  CC="$CC" \
-  AR="$AR" \
-  RANLIB="$RANLIB" \
-  CFLAGS="$CFLAGS" \
-  LDFLAGS="$LDFLAGS"
+  LDFLAGS="-specs=$DEVKITPRO/libnx/switch.specs $LDFLAGS" \
+  CONFIG_SITE="config.site"
+popd
+cp ../cpython_config_files/Setup.local build-switch/Modules
+pushd build-switch
+make -j $(getconf _NPROCESSORS_ONLN) libpython3.9.a
+mkdir -p $LOCAL_PREFIX/lib
+cp libpython3.9.a $LOCAL_PREFIX/lib/libpython3.9.a
+make libinstall
+make inclinstall
+popd
+popd
 
-# Собираем только необходимые модули
-echo "Building Python core..."
-make -j4 python \
-  CC="$CC" \
-  AR="$AR" \
-  CFLAGS="$CFLAGS" \
-  LDFLAGS="" \
-  LIBS="" || true
+mkdir -p ./python39-switch
 
-# Собираем все .o файлы в библиотеку
-echo "Creating libpython3.9.a..."
-find . -name "*.o" -type f | xargs $AR rcs libpython3.9.a
+mv $LOCAL_PREFIX/* ./python39-switch/
 
-# Создаем директории для установки
-mkdir -p ../../python39-switch/lib
-mkdir -p ../../python39-switch/include/python3.9
-
-# Копируем библиотеку
-cp libpython3.9.a ../../python39-switch/lib/
-
-# Копируем заголовки
-cp ../Include/*.h ../../python39-switch/include/python3.9/
-cp pyconfig.h ../../python39-switch/include/python3.9/
-
-echo "=== Verification ==="
-file ../../python39-switch/lib/libpython3.9.a
-echo "Library created successfully"
+pushd python39-switch/lib/python3.9
+rm -r test
+rm -r lib2to3/tests
+rm subprocess.py
+cp ../../../stub/subprocess.py ./
+find . -type l -not -name \*.py -delete
+find . -type d -empty -delete
+find . -name \*.py -exec python3 -OO -m py_compile {} \;
+popd
