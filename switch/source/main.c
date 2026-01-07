40d55a0e1e5397f65588b7084d66d73f5da5c4c7
Gorag <greg6821@gmail.com>
2026-01-07 20:22:04 +0300

    Update main.c

--- switch/source/main.c
+++ switch/source/main.c
@@ -1,67 +1,29 @@
 #include <switch.h>
 #include <Python.h>
-#include <stdio.h>
 
-void userAppInit(void) {
-    // Инициализация romfs и SD-карты
-    fsdevMountSdmc();
-    romfsInit();
+int main(int argc, char* argv[])
+{
+    consoleInit(NULL);
 
-    // Перенаправляем stdout / stderr в файл на SD
-    freopen("sdmc:/renpy_switch.log", "w", stdout);
-    freopen("sdmc:/renpy_switch.log", "w", stderr);
+    Py_SetProgramName(L"python");
+    Py_Initialize();
 
-    printf("=== Python Switch smoketest launcher started ===\n");
-}
-
-void userAppExit(void) {
-    romfsExit();
-}
-
-// Простая функция для sleep из libnx, чтобы проверить модуль _nx
-static PyObject* py_nx_sleep(PyObject* self, PyObject* args) {
-    double seconds;
-    if (!PyArg_ParseTuple(args, "d", &seconds))
-        return NULL;
-
-    uint64_t ns = (uint64_t)(seconds * 1000000000ULL);
-    Py_BEGIN_ALLOW_THREADS
-    svcSleepThread(ns);
-    Py_END_ALLOW_THREADS
-    Py_RETURN_NONE;
-}
-
-int main(int argc, char* argv[]) {
-    userAppInit();
-
-    // Python flags
-    Py_NoSiteFlag = 1;
-    Py_IgnoreEnvironmentFlag = 1;
-    Py_NoUserSiteDirectory = 1;
-    Py_DontWriteBytecodeFlag = 1;
-    Py_OptimizeFlag = 2;
-
-
-    // Инициализация Python
-    Py_InitializeEx(0);
-
-    // Устанавливаем домашнюю директорию
-    setenv("HOME", "/save", 1);
-
-    // Загружаем и выполняем smoketest
-    FILE* test_file = fopen("romfs:/renpy_smoketest.py", "r");
-    if (!test_file) {
-        printf("Could not find renpy_smoketest.py\n");
-        Py_Finalize();
-        userAppExit();
-        return 1;
-    }
-
-    PyRun_SimpleFile(test_file, "renpy_smoketest.py");
-    fclose(test_file);
+    PyRun_SimpleString(
+        "import sys\n"
+        "sys.path.insert(0, 'romfs:/python')\n"
+        "import test_renpy\n"
+    );
 
     Py_Finalize();
-    userAppExit();
 
+    printf("\nDone. Press PLUS to exit.\n");
+    while (appletMainLoop()) {
+        hidScanInput();
+        if (hidKeysDown(CONTROLLER_P1_AUTO) & KEY_PLUS)
+            break;
+        consoleUpdate(NULL);
+    }
+
+    consoleExit(NULL);
     return 0;
 }
