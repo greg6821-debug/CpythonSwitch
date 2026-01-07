979647193b618c6b53e133297cad6ce4e3b4737c
Gorag <greg6821@gmail.com>
2026-01-07 04:42:53 +0300

    Create main.c

---
+++ switch/source/main.c
@@ -0,0 +1,39 @@
+#include <switch.h>
+#include <Python.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    consoleInit(NULL);
+
+    printf("Starting Python...\n");
+
+    Py_SetProgramName(L"python");
+    Py_Initialize();
+
+    FILE *fp = fopen("romfs:/renpy_smoketest.py", "r");
+    if (!fp) {
+        printf("Failed to open test script\n");
+        Py_Finalize();
+        consoleUpdate(NULL);
+        while (appletMainLoop()) svcSleepThread(1e9);
+    }
+
+    PyRun_SimpleFile(fp, "renpy_smoketest.py");
+    fclose(fp);
+
+    Py_Finalize();
+
+    printf("Python finished\n");
+    consoleUpdate(NULL);
+
+    while (appletMainLoop()) {
+        hidScanInput();
+        if (hidKeysDown(CONTROLLER_P1_AUTO) & KEY_PLUS)
+            break;
+        consoleUpdate(NULL);
+    }
+
+    consoleExit(NULL);
+    return 0;
+}
