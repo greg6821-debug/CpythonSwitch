2b43682e1592a9f9e620dbc98b6d9830e3762dfa
Gorag <greg6821@gmail.com>
2026-01-08 01:40:03 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,6 +1,18 @@
 set -e
 
 export DEVKITPRO=/opt/devkitpro
+export DEVKITA64=/opt/devkitpro/devkitA64
+
+# Явно указываем компилятор devkitPro
+export CC="$DEVKITA64/bin/aarch64-none-elf-gcc"
+export CXX="$DEVKITA64/bin/aarch64-none-elf-g++"
+export AR="$DEVKITA64/bin/aarch64-none-elf-ar"
+export RANLIB="$DEVKITA64/bin/aarch64-none-elf-ranlib"
+
+# Флаги для сборки под Switch
+export CFLAGS="-O2 -ffunction-sections -fdata-sections -D__SWITCH__ -I$DEVKITPRO/libnx/include"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-specs=$DEVKITPRO/libnx/switch.specs"
 
 source $DEVKITPRO/switchvars.sh
 pushd cpython
@@ -9,6 +21,8 @@ cp ../cpython_config_files/config.site build-switch
 pushd build-switch
 mkdir local_prefix
 export LOCAL_PREFIX=$(realpath local_prefix)
+
+# Явно передаем компилятор в configure
 ../configure \
   --host=aarch64-none-elf \
   --build=$(../config.guess) \
@@ -16,12 +30,24 @@ export LOCAL_PREFIX=$(realpath local_prefix)
   --disable-ipv6 \
   --disable-shared \
   --without-pymalloc \
-  LDFLAGS="-specs=$DEVKITPRO/libnx/switch.specs $LDFLAGS" \
+  CC="$CC" \
+  AR="$AR" \
+  RANLIB="$RANLIB" \
+  CFLAGS="$CFLAGS" \
+  LDFLAGS="$LDFLAGS" \
   CONFIG_SITE="config.site"
 popd
 cp ../cpython_config_files/Setup.local build-switch/Modules
 pushd build-switch
+
+# Собираем только библиотеку Python
 make -j $(getconf _NPROCESSORS_ONLN) libpython3.9.a
+
+# Проверяем архитектуру
+echo "Проверяем архитектуру библиотеки:"
+file libpython3.9.a
+aarch64-none-elf-objdump -f libpython3.9.a | head -5
+
 mkdir -p $LOCAL_PREFIX/lib
 cp libpython3.9.a $LOCAL_PREFIX/lib/libpython3.9.a
 make libinstall
@@ -30,7 +56,6 @@ popd
 popd
 
 mkdir -p ./python39-switch
-
 mv $LOCAL_PREFIX/* ./python39-switch/
 
 pushd python39-switch/lib/python3.9
@@ -42,3 +67,6 @@ find . -type l -not -name \*.py -delete
 find . -type d -empty -delete
 find . -name \*.py -exec python3 -OO -m py_compile {} \;
 popd
+
+echo "=== Проверяем финальную библиотеку ==="
+file python39-switch/lib/libpython3.9.a
