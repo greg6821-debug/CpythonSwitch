f08a9068ac954ba0323e0a616091980224786fa8
Gorag <greg6821@gmail.com>
2026-01-08 20:44:53 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -5,27 +5,16 @@ export DEVKITPRO=/opt/devkitpro
 export DEVKITA64=$DEVKITPRO/devkitA64
 export PATH=$DEVKITA64/bin:$PATH
 
-export CC=aarch64-none-elf-gcc
-export AR=aarch64-none-elf-ar
-export RANLIB=aarch64-none-elf-ranlib
+echo "[*] Building CPython from git checkout"
 
-export CFLAGS="-O2 -ffunction-sections -fdata-sections"
-export LDFLAGS=""
-
-PYTHON_VERSION=3.9.22
-
-tar xf Python-$PYTHON_VERSION.tar.xz
-cd Python-$PYTHON_VERSION
-
-patch -p1 < ../cpython.patch
-cp ../cpython_config_files/config.site .
+cd cpython
 
 ./configure \
-    --host=aarch64-none-elf \
-    --build=x86_64-linux-gnu \
-    --disable-shared \
-    --enable-ipv6=no \
-    --without-ensurepip \
-    --prefix=/python
+  --host=aarch64-none-elf \
+  --build=x86_64-linux-gnu \
+  --disable-shared \
+  --without-ensurepip \
+  --enable-ipv6=no \
+  --prefix=/python
 
 make -j$(nproc)
