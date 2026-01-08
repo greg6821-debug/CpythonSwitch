8900978e0bab137a5b63171d60963bda29871209
Gorag <greg6821@gmail.com>
2026-01-08 20:20:20 +0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,44 +1,31 @@
+#!/usr/bin/env bash
 set -e
 
 export DEVKITPRO=/opt/devkitpro
+export DEVKITA64=$DEVKITPRO/devkitA64
+export PATH=$DEVKITA64/bin:$PATH
 
-source $DEVKITPRO/switchvars.sh
-pushd cpython
-mkdir build-switch
-cp ../cpython_config_files/config.site build-switch
-pushd build-switch
-mkdir local_prefix
-export LOCAL_PREFIX=$(realpath local_prefix)
-../configure \
-  --host=aarch64-none-elf \
-  --build=$(../config.guess) \
-  --prefix="$LOCAL_PREFIX" \
-  --disable-ipv6 \
-  --disable-shared \
-  --without-pymalloc \
-  LDFLAGS="-specs=$DEVKITPRO/libnx/switch.specs $LDFLAGS" \
-  CONFIG_SITE="config.site"
-popd
-cp ../cpython_config_files/Setup.local build-switch/Modules
-pushd build-switch
-make -j $(getconf _NPROCESSORS_ONLN) libpython3.9.a
-mkdir -p $LOCAL_PREFIX/lib
-cp libpython3.9.a $LOCAL_PREFIX/lib/libpython3.9.a
-make libinstall
-make inclinstall
-popd
-popd
+export CC=aarch64-none-elf-gcc
+export AR=aarch64-none-elf-ar
+export RANLIB=aarch64-none-elf-ranlib
 
-mkdir -p ./python39-switch
+export CFLAGS="-O2 -ffunction-sections -fdata-sections"
+export LDFLAGS=""
 
-mv $LOCAL_PREFIX/* ./python39-switch/
+PYTHON_VERSION=3.9.22
 
-pushd python39-switch/lib/python3.9
-rm -r test
-rm -r lib2to3/tests
-rm subprocess.py
-cp ../../../stub/subprocess.py ./
-find . -type l -not -name \*.py -delete
-find . -type d -empty -delete
-find . -name \*.py -exec python3 -OO -m py_compile {} \;
-popd
+tar xf Python-$PYTHON_VERSION.tar.xz
+cd Python-$PYTHON_VERSION
+
+patch -p1 < ../cpython.patch
+cp ../cpython_config_files/config.site .
+
+./configure \
+    --host=aarch64-none-elf \
+    --build=x86_64-linux-gnu \
+    --disable-shared \
+    --enable-ipv6=no \
+    --without-ensurepip \
+    --prefix=/python
+
+make -j$(nproc)
